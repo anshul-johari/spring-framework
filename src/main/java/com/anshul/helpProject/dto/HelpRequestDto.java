c58c24ffe428799a41780ef3c0e8cb54b7d97119
consint-anshul <anshul.johari@consint.ai>
2025-03-29 16:46:04 +0530

    Changes made

--- src/main/java/com/anshul/helpProject/dto/HelpRequestDto.java
+++ src/main/java/com/anshul/helpProject/dto/HelpRequestDto.java
@@ -1,5 +1,6 @@
 package com.anshul.helpProject.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,14 +10,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class HelpRequestDto {
 	
+	@NotBlank(message = "Title is mandatory.")
 	private String title;
 	
+	@NotBlank(message = "Request Code is mandatory.")
 	private String requestCode;
 	
+	@NotBlank(message = "Discription is mandatory.")
 	private String discription;
 	
 	private String location;
 	
+	@NotBlank(message = "Status is mandatory.")
 	private String status;
 
 }
